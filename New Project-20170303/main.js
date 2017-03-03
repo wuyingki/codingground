e6d6be63042c31146f97b482db509c5b8466c299
Ki <yk_wu@hotmail.com>
2017-03-03 09:32:41 +0800

    Created by Coding Ground Client

---
+++ New Project-20170303/main.js
@@ -0,0 +1,5 @@
+/* Simple Hello World in Node.js */
+console.log("Hello World");
+/* https://www.towngasmiacucina.com/en/showroom/ */
+/* xpath for TST tel html/body/div[1]/div[2]/div[2]/div/div/ul/li[1]/div/div[2]/div/div[1]/ul/li[2]/span */
+/* xpath for MK tel html/body/div[1]/div[2]/div[2]/div/div/ul/li[2]/div/div[2]/div/div[1]/ul/li[2]/span */
